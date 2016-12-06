7b7cc7a9bdcdb4ff5658077029c0ae85df31a930
Gan Quan <coin2028@hotmail.com>
2016-12-06 00:22:03 -0500

    rewrote

--- spark-gd/sparkgd.py
+++ spark-gd/sparkgd.py
@@ -1,73 +1,114 @@
-'''
-Performs full-batch gradient descent with any first-order gradient
-optimization method you like on Spark.  You need to specify
-gradient calculation by your own, of course; this module cannot
-automagically perform symbolic gradient computation.
 
-Remember to add this file into SparkContext.
-'''
+import pyspark
+import numpy as NP
+import copy
+import numbers
 
-def _add_losses(r1, r2):
-    return r1[0] + r2[0], {c: r1[1][c] + r2[1][c] for c in r1[1]}
+class MeanAccumulatorParam(pyspark.AccumulatorParam):
+    def addInPlace(self, v1, v2):
+        v1[1] = (v1[1] * v1[0] + v2[1] * v2[0]) / (v1[0] + v2[0])
+        v1[0] += v2[0]
+        return v1
 
-def step(dataset, model, grad, desc, count=None):
+class MeanLossAccumulatorParam(MeanAccumulatorParam):
+    def zero(self, initialValue):
+        return [0, 0.]
+
+class MeanGradAccumulatorParam(MeanAccumulatorParam):
+    def zero(self, initialValue):
+        return [0, NP.zeros_like(initialValue[1], dtype='float32')]
+
+
+def _get_samples(df, df_count, size):
+    # http://stackoverflow.com/questions/34003314/how-take-a-random-row-from-a-pyspark-dataframe
+    fraction_bound = max(size * 1.5, 100.) / df_count
+    return df.sample(False, fraction_bound).limit(size)
+
+
+def train_model(model, train_set, valid_set, eval_, grad, desc,
+                batch_size=256, times_per_train_epoch=None,
+                times_per_valid_epoch=None, max_epochs=None,
+                patience=50, patience_increase=2,
+                improvement_threshold=0.995):
     '''
-    Perform one full-batch gradient descent step.
-
-    Parameters
-    ----------
-    dataset : RDD instance of any type
-        An RDD of any type.
-    model : dict
-        A dict of any key-value type.  Usually you want to use parameter names
-        as keys and parameter values as dict values.
-    grad : callable
-        User-provided function which computes per-data-point loss and
-        gradient.
-        
-        It should take two arguments:
-        data_point : any
-            A data point, which is of the same type as the elements in
-            @dataset.
-        model : dict
-            Your value of @model would be passed into this argument.
-
-        It should return two values:
-        loss : number
-            A number indicating the loss on this data point.
-        grads : dict
-            A dict with the same keys as @model, and the values the gradient
-            w.r.t. each parameter with the same key.
-    desc : callable
-        User-provided function which performs the actual descent operation.
-
-        It should take two values:
-        model
-            Your value of @model would be passed into this argument.  Note
-            that the content there would be changed when desc() returns.
-        grads
-            Your result of @grad() function would be passed into this
-            argument.
-
-        In this function you should update your model accordingly by
-        updating the values in @model.
-    count : int or None
-        The number of elements in the dataset.  If given None, step()
-        would count the number of elements each time you call it.
-
-    Returns
-    -------
-    loss : number
-        The average loss over the entire dataset.
+    model : dict of {param_name: param_value}
+    train_set, valid_set : DataFrame
+    eval_ : func (data_point, model) -> (metric) (the smaller the better)
+    grad : func (data_point, model) -> (loss, {param_name: param_grad})
+    desc : func (model, {param_name: param_grad}) -> ()
     '''
-    sc = dataset.context
-    if count is None:
-        count = dataset.count()
-    _model_broadcast = sc.broadcast(model)
-    preds = dataset.map(lambda r: grad(r, _model_broadcast))
-    agg = preds.treeReduce(_add_losses)
-    loss = agg[0] / count
-    grads = {c: agg[1][c] / count for c in agg[1]}
-    _model_broadcast.unpersist()
-    desc(model, grads)
-    return loss
+    train_count = train_set.count()
+    valid_count = valid_set.count()
+
+    if times_per_train_epoch is None:
+        times_per_train_epoch = train_count / batch_size
+    if times_per_valid_epoch is None:
+        times_per_valid_epoch = valid_count / batch_size
+    if max_epochs is None:
+        max_epochs = NP.inf
+
+    sc = train_set.rdd.context
+
+    # http://deeplearning.net/tutorial/gettingstarted.html#early-stopping
+    epoch = 0
+    best_model = None
+    best_valid_loss = NP.inf
+
+    while epoch < max_epochs:
+        epoch += 1
+
+        for batch in range(times_per_train_epoch):
+            train_batch = (_get_samples(train_set, train_count, batch_size)
+                           .repartition(batch_size))
+
+            model_bc = {c: sc.broadcast(model[c]) for c in model}
+            loss_acc = sc.accumulator([0, 0.], MeanLossAccumulatorParam())
+            grad_acc = {c: sc.accumulator([0, NP.zeros_like(model[c], dtype='float32')],
+                                          MeanGradAccumulatorParam())
+                        for c in model}
+
+            def _accum_losses(r, loss_acc=loss_acc, grad_acc=grad_acc,
+                              model_bc=model_bc):
+                loss, grads = grad(r, {c: model_bc[c].value for c in model})
+                print ('Accumulating loss %f' % loss)
+                loss_acc.add([1, loss])
+                for c in grads:
+                    print ('Accumulating gradient w.r.t. %s' % c)
+                    grad_acc[c].add([1, grads[c]])
+
+            train_batch.foreach(_accum_losses)
+            print '#%-8d@%-9d%f' % (epoch, batch, loss_acc.value[1])
+
+            grads = {c: grad_acc[c].value[1] for c in model}
+            desc(model, grads)
+
+            for c in model_bc:
+                model_bc[c].unpersist()
+
+        valid_loss = 0
+        for batch in range(times_per_valid_epoch):
+            valid_batch = (_get_samples(valid_set, valid_count, batch_size)
+                           .repartition(batch_size))
+
+            model_bc = {c: sc.broadcast(model[c]) for c in model}
+            loss_acc = sc.accumulator([0, 0.], MeanLossAccumulatorParam())
+
+            def _accum_losses(r, loss_acc=loss_acc, model_bc=model_bc):
+                loss = eval_(r, {c: model_bc[c].value for c in model})
+                print ('Accumulating validation loss %f' % loss)
+                loss_acc.add([1, loss])
+
+            valid_batch.foreach(_accum_losses)
+            valid_loss += loss_acc.value[1]
+        valid_loss /= times_per_valid_epoch
+        print '#%-8d -> %f' % (epoch, valid_loss)
+
+        if valid_loss < best_valid_loss:
+            if valid_loss < best_valid_loss * improvement_threshold:
+                patience = max(patience, epoch * patience_increase)
+            best_model = copy.deepcopy(model)
+            best_valid_loss = valid_loss
+
+        if patience <= epoch:
+            print '>%-8d -> %f' % (epoch, valid_loss)
+            break
